0684160c9746848bbedd749bd92d9f1c47f0ce25
TH <kareme007@gmail.com>
2024-01-19 13:10:37 +0900

    refactor : UserMapperTest

--- src/test/java/com/baby/babycareproductsshop/user/UserMapperTest.java
+++ src/test/java/com/baby/babycareproductsshop/user/UserMapperTest.java
@@ -1,36 +1,89 @@
 package com.baby.babycareproductsshop.user;
 
+import com.baby.babycareproductsshop.user.model.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserMapperTest {
+    @Autowired
+    private UserMapper mapper;
 
     @Test
-    void insUser() {
+    @DisplayName("ins user test")
+    void insUser() {    //39
+        UserSignUpDto dto = new UserSignUpDto();
+        dto.setUid("test1122");
+        dto.setUpw("asldnalkdfnlksdn!@3");
+        dto.setNm("연보라");
+        dto.setPhoneNumber("010-7777-8888");
+        dto.setEmail("test1122@naver.com");
+        int insResult = mapper.insUser(dto);
+        assertEquals(1, insResult);
     }
 
     @Test
+    @DisplayName("sel my info test")
     void selMyInfo() {
+        int iuser = 38;
+        UserSelMyInfoVo vo = mapper.selMyInfo(iuser);
+        assertEquals("연보라", vo.getNm());
     }
 
     @Test
+    @DisplayName("sel sign in info by uid")
     void selSignInInfoByUid() {
+        String uid = "winter";
+        UserSignInProcDto dto = mapper.selSignInInfoByUid(uid);
+        assertNotNull(dto);
+        assertEquals(20 ,dto.getIuser());
     }
 
     @Test
+    @DisplayName("sel user info by iuser")
     void selUserInfoByIuser() {
+        int iuser = 19;
+        UserSelToModifyVo vo = mapper.selUserInfoByIuser(iuser);
+        assertEquals("하늘색", vo.getNm());
+        assertEquals("011-0010-3924", vo.getPhoneNumber());
     }
 
     @Test
+    @DisplayName("sel clause test")
     void selClause() {
+        List<UserClauseVo> list = mapper.selClause();
+        assertEquals(2, list.size());
     }
 
     @Test
+    @DisplayName("del user test")
     void delUser() {
+        int iuser = 27;
+        int delResult = mapper.delUser(iuser);
+        assertEquals(1, delResult);
+
+        int iuser2 = 2;
+        int delResult2 = mapper.delUser(iuser2);
+        assertEquals(1, delResult2);
     }
 
     @Test
     void updUser() {
+        UserUpdDto dto = new UserUpdDto();
+        dto.setIuser(5);
+        dto.setNm("자바");
+        dto.setPhoneNumber("010-7777-4444");
+        dto.setEmail("javax@gmail.com");
+        int updResult = mapper.updUser(dto);
+        assertEquals(1, updResult);
     }
 }
